078a4729ea401eb2c2032c657f9a8a801e21a9db
akashshinde7219 <akash.shinde@realthingks.com>
2025-07-21 15:21:18 +0530

    changes in deployment

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'a5db7f63410944106b7170cb109eca00-87888423.us-east-1.elb.amazonaws.com'
+    allowedHosts: [
+      'a5db7f63410944106b7170cb109eca00-87888423.us-east-1.elb.amazonaws.com'
+    ]
   }
 })
