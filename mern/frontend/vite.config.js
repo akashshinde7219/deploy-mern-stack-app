264f1fae90ea63efa766453d476444395e310704
akashshinde7219 <akash.shinde@realthingks.com>
2025-07-21 14:58:01 +0530

    vite config changes

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all'
+    allowedHosts: 'a5db7f63410944106b7170cb109eca00-87888423.us-east-1.elb.amazonaws.com'
   }
 })
